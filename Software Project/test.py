f91a91376fc26df0b3bf5057f1681e2b0ac67fdf
Md. Alif Biswas <md.alifbiswas@gmail.com>
2019-02-12 15:17:01 +0600

    Add files via upload

--- Software Project/test.py
+++ Software Project/test.py
@@ -1,14 +1,36 @@
+import os
+import tkinter.messagebox
 from tkinter import *
+from tkinter import filedialog
 from pygame import mixer
 
 
 def playMusic():
-    mixer.music.load("musicTitanic.mp3")
-    mixer.music.play()
+    try:
+        paused  # if pause is initialized or not
+    except NameError:
+        try:
+            mixer.music.load(fileName)
+            mixer.music.play()
+            statusBar['text'] = "now playing       " + \
+                os.path.basename(fileName)
+        except:
+            tkinter.messagebox.showerror("Error", "No music is added")
+    else:
+        mixer.music.unpause()
+        statusBar['text'] = "Music Resumed"
+
+
+def pauseMusic():
+    global paused
+    paused = True
+    mixer.music.pause()
+    statusBar['text'] = "Music Paused"
 
 
 def stopMusic():
     mixer.music.stop()
+    statusBar['text'] = "Music Stopped"
 
 
 def setVolume(val):
@@ -17,9 +39,23 @@ def setVolume(val):
     mixer.music.set_volume(volume)
 
 
+def about_us():
+    tkinter.messagebox.showinfo("About This Player", "Alif Biswas || #1502060")
+
+
+def browse_file():
+    global fileName
+    fileName = filedialog.askopenfilename()
+
+
 # Initializing Everything
 root = Tk()
 mixer.init()
+
+# Top Bar & Window Size
+root.geometry('600x400')
+root.title("Furu's Music Player")
+root.iconbitmap('fmp.ico')
 
 # Create the menubar
 menubar = Menu(root)
@@ -28,19 +64,15 @@ root.config(menu=menubar)
 # Create a subMenu
 subMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label='File', menu=subMenu)
-subMenu.add_command(label='New Project')
-subMenu.add_command(label='Exit')
+subMenu.add_command(label='Open', command=browse_file)
+subMenu.add_command(label='Exit', command=root.destroy)
 
 # Create a subMenu
 subMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label='Help', menu=subMenu)
-subMenu.add_command(label='About Us')
+subMenu.add_command(label='About Us', command=about_us)
 subMenu.add_command(label='Contact')
 
-# Top Bar & Window Size
-root.geometry('600x400')
-root.title("Furu's Music Player")
-root.iconbitmap('fmp.ico')
 
 # Play Button
 playPhoto = PhotoImage(file='play.png')
@@ -52,12 +84,22 @@ stopPhoto = PhotoImage(file='stop.png')
 stopBtn = Button(root, image=stopPhoto, command=stopMusic)
 stopBtn.pack()
 
+# Pause Button
+pausePhoto = PhotoImage(file='pause.png')
+pauseBtn = Button(root, image=pausePhoto, command=pauseMusic)
+pauseBtn.pack()
+
 # Volume Scale
 initVolume = 10
 scale = Scale(root, from_=0, to=100, orient=HORIZONTAL, command=setVolume)
 scale.set(initVolume)
 mixer.music.set_volume(initVolume/100)
 scale.pack()
+
+# Create a status bar
+statusBar = Label(root, text='Welcome to Music Player',
+                  relief=SUNKEN, anchor=W)
+statusBar.pack(side=BOTTOM, fill=X)
 
 
 root.mainloop()
